1962bf38a797ecfb02a48d2951f8e951c3267c75
jmrrgncpz <paz.jesr@gmail.com>
2022-01-30 20:35:25 +0800

    fix tag layout

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,19 +1,26 @@
 <template>
-    <span class="text-xs md:text-sm tag bg-primary-500 text-white mr-2 mb-2 rounded-3xl flex-shrink-0 capitalize" :class="customClass">
-        <img v-if="!!imgSrc" :src="imgSrc" class="rounded-full w-8 mr-2 shadow-lg inline">
-        {{ label }}
+  <div
+    class="text-xs md:text-sm tag bg-primary-500 text-white mr-2 mb-2 rounded-3xl flex-shrink-0 capitalize inline-flex flex-row items-center"
+    :class="customClass"
+  >
+    <img
+      v-if="!!imgSrc"
+      :src="imgSrc"
+      class="rounded-full w-8 mr-2 shadow-lg inline"
+    />
+    <span>
+      {{ label }}
     </span>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['label', 'imgSrc'],
-    computed: {
-        customClass() {
-            return [
-                !this.imgSrc ? 'p-3' : 'p-1 pr-3'
-            ]
-        }
-    }
-}
+  props: ["label", "imgSrc"],
+  computed: {
+    customClass() {
+      return [!this.imgSrc ? "p-3" : "p-1 pr-3"];
+    },
+  },
+};
 </script>
